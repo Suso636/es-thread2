f99a04081cb1f6146a8269fa09b99c74d62168ab
student <student@itismeucci.com>
2026-09-23 13:15:12 +0200

    stary

---
+++ src/main/java/it/thereaper/thread/Contatore.java
@@ -0,0 +1,5 @@
+package it.thereaper.thread;
+
+public class Contatore {
+
+}
---
+++ src/main/java/it/thereaper/thread/Main.java
@@ -0,0 +1,136 @@
+package it.thereaper.thread;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+                System.out.println("Hello world!");
+
+                        System.out.println("Hello world!");
+
+                                System.out.println("Hello world!");
+
+                                        System.out.println("Hello world!");
+
+                                                System.out.println("Hello world!");
+
+                                                        System.out.println("Hello world!");
+
+                                                                System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+                System.out.println("Hello world!");
+
+                        System.out.println("Hello world!");
+
+
+                                System.out.println("Hello world!");
+
+
+                                        System.out.println("Hello world!");
+
+                                                System.out.println("Hello world!");
+
+                                                        System.out.println("Hello world!");
+
+                                                                System.out.println("Hello world!");
+
+                                                                        System.out.println("Hello world!");
+
+                                                                                System.out.println("Hello world!");
+
+                                                                                        System.out.println("Hello world!");
+
+                                                                                                System.out.println("Hello world!");
+
+                                                                                                        System.out.println("Hello world!");
+
+                                                                                                                System.out.println("Hello world!");
+
+                                                                                                                        System.out.println("Hello world!");
+
+                                                                                                                                System.out.println("Hello world!");
+
+                                                                                                                                        System.out.println("Hello world!");
+
+                                                                                                                                                System.out.println("Hello world!");
+
+                                                                                                                                                        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+                System.out.println("Hello world!");
+
+                        System.out.println("Hello world!");
+
+
+                                System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+                System.out.println("Hello world!");
+
+                        System.out.println("Hello world!");
+
+                                System.out.println("Hello world!");
+
+                                        System.out.println("Hello world!");
+
+                                                System.out.println("Hello world!");
+
+
+                                                        System.out.println("Hello world!");
+
+
+                                                                System.out.println("Hello world!");
+
+
+
+                                                                        System.out.println("Hello world!");
+
+
+                                                                                System.out.println("Hello world!");
+
+
+                                                                                        System.out.println("Hello world!");
+
+                                                                                                System.out.println("Hello world!");
+
+                                                                                                        System.out.println("Hello world!");
+
+                                                                                                                System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+                System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+                System.out.println("Hello world!");
+
+                        System.out.println("Hello world!");
+
+                                System.out.println("Hello world!");
+
+                                        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+    }
+}
